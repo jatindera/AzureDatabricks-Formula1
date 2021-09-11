c44da4d006278f888ac9c1e2700a439dad3b5492
jatindera@gmail.com <jatindera@gmail.com>
2021-09-11 09:02:18 +0000

    Mount Configuration
    Mount Configuration

--- Mount.py.py
+++ Mount.py.py
@@ -1,2 +1,36 @@
 # Databricks notebook source
-print("Hello, this is a mount")
+# Python code to mount and access Azure Data Lake Storage Gen2 Account to Azure Databricks with Service Principal and OAuth
+# Author: Jatinder Aroar
+
+# Define the variables used for creating connection strings
+adlsAccountName = "adls2pigolu"
+adlsContainerName = "raw"
+adlsFolderName = "formula1"
+mountPoint = "/mnt/raw/formula1"
+
+# Application (Client) ID
+applicationId = dbutils.secrets.get(scope="secretscope-pigolu-kv",key="sp-pigolu-formula1-clientid")
+
+# Application (Client) Secret Key
+authenticationKey = dbutils.secrets.get(scope="secretscope-pigolu-kv",key="sp-pigolu-formula1-secret")
+
+# Directory (Tenant) ID
+tenandId = dbutils.secrets.get(scope="CSVProjectKeyVault",key="tenantid")
+
+endpoint = "https://login.microsoftonline.com/" + tenandId + "/oauth2/token"
+source = "abfss://" + adlsContainerName + "@" + adlsAccountName + ".dfs.core.windows.net/" + adlsFolderName
+
+# Connecting using Service Principal secrets and OAuth
+configs = {"fs.azure.account.auth.type": "OAuth",
+           "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+           "fs.azure.account.oauth2.client.id": applicationId,
+           "fs.azure.account.oauth2.client.secret": authenticationKey,
+           "fs.azure.account.oauth2.client.endpoint": endpoint}
+
+# Mounting ADLS Storage to DBFS
+# Mount only if the directory is not already mounted
+if not any(mount.mountPoint == mountPoint for mount in dbutils.fs.mounts()):
+  dbutils.fs.mount(
+    source = source,
+    mount_point = mountPoint,
+    extra_configs = configs)
